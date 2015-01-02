8b2c11670b963c51f3085bd34f7ed4d5622d02fa
Mushkin.V <mushkin.v@gmail.com>
2015-01-02 19:16:35 +0200

    add mod FOSUser

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,8 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,9 +20,56 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="card_number", type="integer")
+     *
+     * @Assert\Range(
+     *      min = 1000,
+     *      max = 9999,
+     *      minMessage = "Your card number must be minimum {{ limit }} ",
+     *      maxMessage = "Your card number must be maximum {{ limit }} "
+     * )
+     */
+    private $card_number;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set card_number
+     *
+     * @param integer $cardNumber
+     * @return User
+     */
+    public function setCardNumber($cardNumber)
+    {
+        $this->card_number = $cardNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get card_number
+     *
+     * @return integer 
+     */
+    public function getCardNumber()
+    {
+        return $this->card_number;
     }
 }
---
+++ src/AppBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+public function buildForm(FormBuilderInterface $builder, array $options)
+{
+parent::buildForm($builder, $options);
+
+$builder->add('card_number');
+}
+
+public function getName()
+{
+return 'vit_user_registration';
+}
+}
