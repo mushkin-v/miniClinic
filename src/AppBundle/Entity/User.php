0a3f5be818c2f46ad5c68e65e7e31ac82989dae5
Mushkin.V <mushkin.v@gmail.com>
2015-01-02 19:53:08 +0200

    add unique field to User

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -6,10 +6,12 @@ use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ *  * @UniqueEntity("card_number")
  */
 class User extends BaseUser
 {
@@ -23,7 +25,9 @@ class User extends BaseUser
     /**
      * @var integer
      *
-     * @ORM\Column(name="card_number", type="integer")
+     * @ORM\Column(name="card_number", type="integer", unique=true)
+     *
+     * @Assert\NotBlank()
      *
      * @Assert\Range(
      *      min = 1000,
